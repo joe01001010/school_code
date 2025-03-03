bd7b9fe126eb823a9cb43285783e73ad9edb493f
joe01001010 <jweibel@uccs.edu>
2025-03-02 18:25:18 -0700

    Added a function that shows examples of binary tree traversal

---
+++ Python/binary_tree_traversal.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def inorder(self, node):
+        if node:
+            self.inorder(node.left)
+            print(node.data, end=' ')
+            self.inorder(node.right)
+
+    def preorder(self, node):
+        if node:
+            print(node.data, end=' ')
+            self.preorder(node.left)
+            self.preorder(node.right)
+
+    def postorder(self, node):
+        if node:
+            self.postorder(node.left)
+            self.postorder(node.right)
+            print(node.data, end=' ')
+
+def main():
+    tree = BinaryTree(1)
+    tree.root.left = Node(2)
+    tree.root.right = Node(3)
+    tree.root.left.left = Node(4)
+    tree.root.left.right = Node(5)
+    tree.root.right.left = Node(6)
+    tree.root.right.right = Node(7)
+
+    print("Inorder traversal:")
+    tree.inorder(tree.root)
+    print("\nPreorder traversal:")
+    tree.preorder(tree.root)
+    print("\nPostorder traversal:")
+    tree.postorder(tree.root)
+
+if __name__ == "__main__":
+    main()
