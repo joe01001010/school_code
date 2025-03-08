c9192d11710d2869bd23615dd808f2bafc2db554
joe01001010 <jweibel@uccs.edu>
2025-03-08 12:58:37 -0700

    Got the website working and recoded it so the website is dark mode. Waiting for a reply from my professor to see if I need to use a different method for scraping Google's website.

--- Python/weather_app/weather_app.py
+++ Python/weather_app/weather_app.py
@@ -1,11 +1,25 @@
+import requests
 from flask import Flask, request, render_template
-
 app = Flask(__name__)
+
+
+def main():
+    app.run(debug=True)
+    form()
+    website_list = [
+        'https://www.google.com/search?q=Colorado+Springs+Colorado+USA+weather',
+        'https://www.google.com/search?q=Winter+Park+Colorado+USA+weather',
+        'https://www.google.com/search?q=Winter+Park+Florida+USA+weather',
+        'https://www.google.com/search?q=Alexandria+Virginia+USA+weather',
+        'https://www.google.com/search?q=Alexandria+Ontario+Canada+weather',
+        'https://www.google.com/search?q=Melbourne+Florida+USA+weather',
+        'https://www.google.com/search?q=Melbourne+Victoria+Australia+weather'
+    ]
+
 
 @app.route('/', methods=['GET', 'POST'])
 def form():
     if request.method == 'POST':
-        # Get form data
         first_name = request.form.get('first_name')
         last_name = request.form.get('last_name')
         city = request.form.get('city')
@@ -22,4 +36,4 @@ def form():
     return render_template('weather_app_template.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    main()
