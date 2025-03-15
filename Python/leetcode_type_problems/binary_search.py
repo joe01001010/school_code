a8b5cac6ce752797c490ff0a5c54d94f63af070b
joe01001010 <jweibel@uccs.edu>
2025-03-15 15:16:24 -0600

    Made a binary search program.

---
+++ Python/leetcode_type_problems/binary_search.py
@@ -0,0 +1,30 @@
+def binary_search(num_list, num_to_find):
+    left = 0
+    right = len(num_list) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if num_list[mid] > num_to_find:
+            right = mid - 1
+        elif num_list[mid] < num_to_find:
+            left = mid + 1
+        else:
+            return mid
+    return -1
+
+
+def main():
+    num_to_find = 99997
+    print("Target number:", num_to_find)
+    num_list = []
+    for i in range(1,100001,2):
+        num_list.append(i)
+        
+    index_for_num = binary_search(num_list, num_to_find)
+    if index_for_num != -1:
+        print(f"The index for {num_list[index_for_num]} is: {index_for_num}")
+    else:
+        print("The target number is not in the list")
+
+
+if __name__ == '__main__':
+    main()
