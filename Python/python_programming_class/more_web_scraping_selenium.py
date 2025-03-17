799a1eaf580a710b0ba683c24417920bd3e927f6
joe01001010 <jweibel@uccs.edu>
2025-03-17 16:09:53 -0600

    Added the list to test how it will work in my main application for class

--- Python/python_programming_class/more_web_scraping_selenium.py
+++ Python/python_programming_class/more_web_scraping_selenium.py
@@ -1,8 +1,20 @@
 import undetected_chromedriver as uc
+import time
 
 def main():
+    website_list = [
+        'https://www.google.com/search?q=Colorado+Springs+Colorado+USA+weather',
+        'https://www.google.com/search?q=Winter+Park+Colorado+USA+weather',
+        'https://www.google.com/search?q=Winter+Park+Florida+USA+weather',
+        'https://www.google.com/search?q=Alexandria+Virginia+USA+weather',
+        'https://www.google.com/search?q=Alexandria+Ontario+Canada+weather',
+        'https://www.google.com/search?q=Melbourne+Florida+USA+weather',
+        'https://www.google.com/search?q=Melbourne+Victoria+Australia+weather'
+    ]
     browser = uc.Chrome()
-    browser.get('https://www.google.com/search?q=Colorado+Springs+Colorado+USA+weather')
+    for website in website_list:
+        browser.get(website)
+        time.sleep(2)
 
     input("Press enter to close browser")
     browser.quit()
