38ae450dc8014c5fcdb655139c1fb5a90ffca4e6
Joe <joeweibel77@gmail.com>
2025-03-27 21:34:42 -0600

    Added a new function to test binary search to look for boolean values

---
+++ Python/leetcode_type_problems/binary_search_alt.py
@@ -0,0 +1,20 @@
+def main():
+    first_list = [False] * 24
+    second_list = [True] * 2
+    total_list = first_list + second_list
+    left = 0
+    right = len(total_list) - 1
+    while left < right:
+        mid = (left + right) // 2
+        print(f"Left: {left}")
+        print(f"Right: {right}")
+        print(f"Mid: {mid}")
+        print()
+        if total_list[mid]:
+            right = mid
+        else:
+            left = mid + 1
+
+
+if __name__ == "__main__":
+    main()
