788ce26530f88299eff771f11ce2ce03df919d8b
joe01001010 <jweibel@uccs.edu>
2025-04-04 12:53:53 -0600

    Deleted comments from the Django app

--- Python/python_programming_class/my_tennis_club/members/views.py
+++ Python/python_programming_class/my_tennis_club/members/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-# Create your views here.
 
 def members(request):
     template = loader.get_template('myfirst.html')
